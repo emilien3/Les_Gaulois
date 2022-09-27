e53587b92cb3748a784dbc12dec01b56267938ff
sanchez emilien <snm4372a@ens.fsi>
2022-09-27 19:55:25 +0200

    je me suis arreter slide 19, il y a un pb avec les class peut qu'il faut un fichier par class ???

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -5,5 +5,108 @@ public class Gaulois {
 	private String nom;
 	private int force ;
 	private int effetPotion = 1;
+	
+	
+	
+	// Auto-generated method stub
 
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	
+	// End of Auto-generated method
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+		+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	
+	public static void main(String[] args) {
+	}
+	
 }
+
+
+public class Romain {
+	
+	private String nom;
+	private int force;
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
+}
+
+
+public class Druide {
+	
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller
+				d'une force " + effetPotionMin + " à "
+				+ effetPotionMax + ".");
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+}
+
+
+
+
+
